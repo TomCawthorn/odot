5b8fb32828152cf15a07dff03b8f2640a5c47f5a
Tom Cawthorn <tom_cawthorn@btinternet.com>
2014-06-30 20:48:26 +0100

    Delete todo list items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,18 +1,16 @@
 class TodoItemsController < ApplicationController
-  def index
-      
-      @todo_list = TodoList.find(params[:todo_list_id])
-  end
+  before_action :find_todo_list
+    
+    def index
+    end
     
     
     def new
-       @todo_list = TodoList.find(params[:todo_list_id]) 
        @todo_item = @todo_list.todo_items.new
     end
     
     
     def create
-       @todo_list = TodoList.find(params[:todo_list_id]) 
        @todo_item = @todo_list.todo_items.new(todo_item_params)
         
         if @todo_item.save
@@ -26,14 +24,12 @@ class TodoItemsController < ApplicationController
     
     
     def edit
-       @todo_list = TodoList.find(params[:todo_list_id]) 
        @todo_item = @todo_list.todo_items.find(params[:id])  
     end
     
     
     
   def update
-       @todo_list = TodoList.find(params[:todo_list_id]) 
        @todo_item = @todo_list.todo_items.find(params[:id])
         
         if @todo_item.update_attributes(todo_item_params)
@@ -43,6 +39,17 @@ class TodoItemsController < ApplicationController
             flash[:error] = "That todo item could not be saved"
             render action: :edit
         end
+    end
+    
+    
+    def destroy
+        @todo_item = @todo_list.todo_items.find(params[:id])
+        if @todo_item.destroy
+            flash[:success] = "Todo list item was deleted"
+        else
+            flash[:error] = "Todo list item could not be deleted"
+        end
+        redirect_to todo_list_todo_items_path
     end
     
     
@@ -56,6 +63,11 @@ class TodoItemsController < ApplicationController
     
     
     private
+    
+    def find_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])   
+    end
+    
     def todo_item_params
         params[:todo_item].permit(:content)
     end
--- spec/features/todo_lists/destroy_spec.rb
+++ spec/features/todo_lists/destroy_spec.rb
@@ -4,11 +4,15 @@ describe "Deleting todo lists" do
     let!(:todo_list) {
         todo_list = TodoList.create(title: "Groceries", 
                                     description: "Grovery List.")
+    let!(:todo_item) { todo_list.todo_items.create(content:"Milk") }
     }
+    
+    
+    
     it "Is successful when clicking the destroy link" do
         visit "/todo_lists"  
         within "#todo_list_#{ todo_list.id }" do
-            click_link "Destroy"
+            click_link "Delete"
         end
         expect(page).to_not have_content(todo_list.title)
         expect(TodoList.count).to eq(0)      
