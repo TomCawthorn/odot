85c8e88a2b0dc3fbd13bc77d3c57640d87068c16
Tom Cawthorn <tom_cawthorn@btinternet.com>
2014-06-30 21:05:17 +0100

    Add Rails DOM id helper

--- spec/features/todo_lists/destroy_spec.rb
+++ spec/features/todo_lists/destroy_spec.rb
@@ -1,20 +1,15 @@
 require 'spec_helper'
 
 describe "Deleting todo lists" do
-    let!(:todo_list) {
-        todo_list = TodoList.create(title: "Groceries", 
-                                    description: "Grovery List.")
-    let!(:todo_item) { todo_list.todo_items.create(content:"Milk") }
-    }
-    
-    
-    
-    it "Is successful when clicking the destroy link" do
-        visit "/todo_lists"  
-        within "#todo_list_#{ todo_list.id }" do
-            click_link "Delete"
-        end
-        expect(page).to_not have_content(todo_list.title)
-        expect(TodoList.count).to eq(0)      
-    end  
+  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.") }
+
+  it "is successful when clicking the destroy link" do
+    visit "/todo_lists"
+
+    within "#todo_list_#{todo_list.id}" do
+      click_link "Destroy"
+    end
+    expect(page).to_not have_content(todo_list.title)
+    expect(TodoList.count).to eq(0)
+  end
 end
